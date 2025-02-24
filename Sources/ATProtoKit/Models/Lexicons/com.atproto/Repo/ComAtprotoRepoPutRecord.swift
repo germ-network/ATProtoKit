abb010922fc8b8accb2b6aad161a5596f77e4db5
Anna M <anna@germ.network>
2025-02-24 09:34:49 -0800

    Add init

--- Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/ComAtprotoRepoPutRecord.swift
+++ Sources/ATProtoKit/Models/Lexicons/com.atproto/Repo/ComAtprotoRepoPutRecord.swift
@@ -72,6 +72,24 @@ extension ComAtprotoLexicon.Repository {
             case swapRecord
             case swapCommit
         }
+        
+        public init(
+           repository: String,
+           collection: String,
+           recordKey: String,
+           shouldValidate: Bool = true,
+           record: UnknownType,
+           swapRecord: String? = nil,
+           swapCommit: String? = nil,
+       ) {
+           self.repository = repository
+           self.collection = collection
+           self.recordKey = recordKey
+           self.shouldValidate = shouldValidate
+           self.record = record
+           self.swapRecord = swapRecord
+           self.swapCommit = swapCommit
+       }
     }
 
     /// A output model for creating a record that replaces a previous record.
