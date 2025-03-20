8de7b1d0a5ce3a73d8ad5963334d202d0af52c36
Anna M <anna@germ.network>
2025-03-20 11:57:53 -0700

    Update type to match atproto response

--- Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/AppBskyActorProfile.swift
+++ Sources/ATProtoKit/Models/Lexicons/app.bsky/Actor/AppBskyActorProfile.swift
@@ -45,7 +45,7 @@ extension AppBskyLexicon.Actor {
         ///
         /// - Note: According to the AT Protocol specifications: "Small image to be displayed next
         /// to posts from account. AKA, 'profile picture'"
-        public let avatarBlob: ComAtprotoLexicon.Repository.BlobContainer?
+        public let avatarBlob: ComAtprotoLexicon.Repository.UploadBlobOutput?
 
         /// The banner image URL of the profile. Optional.
         ///
@@ -55,7 +55,7 @@ extension AppBskyLexicon.Actor {
         ///
         /// - Note: According to the AT Protocol specifications: "Larger horizontal image to
         /// display behind profile view."
-        public let bannerBlob: ComAtprotoLexicon.Repository.BlobContainer?
+        public let bannerBlob: ComAtprotoLexicon.Repository.UploadBlobOutput?
 
         /// An array of user-defined labels. Optional.
         ///
